496f24ffd76a42dcd6aceb0ac52e59b7df700e4e
camysandhu <fmsharmn@gmail.com>
2019-10-11 21:13:39 -0400

    Initial Commit

---
+++ PayRoll_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PayRoll_Swift
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
