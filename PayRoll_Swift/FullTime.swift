48a3db09cb07365a14840678fb2c3a411f1da26e
Komal2006 <komaldeepkr99@gmail.com>
2019-10-11 21:28:26 -0400

    FullTIme Class created

---
+++ PayRoll_Swift/FullTime.swift
@@ -0,0 +1,13 @@
+//
+//  FullTime.swift
+//  PayRoll_Swift
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class FullTime
+{
+   print("class FullTime is created")
+}
