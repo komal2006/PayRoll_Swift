e5bbd40d5f7fd6956c1a9aaf38711f94f27b31c2
Komal2006 <komaldeepkr99@gmail.com>
2019-10-15 18:42:41 -0400

    [komal] PartTime  class created

---
+++ PayRoll_Swift/PartTime.swift
@@ -0,0 +1,13 @@
+//
+//  PartTime.swift
+//  PayRoll_Swift
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class PartTime
+{
+    
+}
